3712ea21e54e5d32528ecd6e9e6b563b609486fe
OmarGurbanli <73650963+OmarGurbanli@users.noreply.github.com>
2025-05-08 00:14:59 +0300

    Comments change App.java

--- app/App.java
+++ app/App.java
@@ -9,16 +9,16 @@ import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) {
-        // Показываем LoginDialog один раз
+        // LoginDialog seen just once
         LoginDialog login = new LoginDialog();
         login.setVisible(true);
 
-        // Если логин не прошёл — выходим
+        // if login unsuccessful ->exit
         if (AppContext.getCurrentUser() == null) {
             System.exit(0);
         }
 
-        // Иначе — открываем MainFrame
+        // otherwise open MainFrame
         new MainFrame().setVisible(true);
     }
 }
